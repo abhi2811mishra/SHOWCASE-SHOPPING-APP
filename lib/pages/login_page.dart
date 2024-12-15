ff421261211e4c42eff5b75bff47668b304d3af7
abhi2811mishra <24ucc203@lnmiit.ac.in>
2024-12-15 22:03:17 +0530

    Day6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,64 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    //function
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+
+  @override
+  Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: SingleChildScrollView(
-          child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_page.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Text(
-            "WELCOME",
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
+      child: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                // Login Image
+                Image.asset(
+                  "assets/images/login_page.png",
+                  fit: BoxFit.cover,
+                  height: 200, // Set a fixed height
+                ),
+                SizedBox(height: 30.0),
+
+                // Welcome Text
+                Text(
+                  "WELCOME $name ",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30.0),
+
+                // Username Field
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter your username",
+                    labelText: "Username",
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {
+                      
+                    });
+                  },
+                ),
+
+                SizedBox(height: 20.0),
+
+                // Password Field
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter your password",
+                    labelText: "Password",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+
+                // Login Button
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    backgroundColor: Colors.blueAccent,
+                    minimumSize: const Size.fromHeight(50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 30.0,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter username",
-              labelText: "Username",
-            ),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "Enter Password",
-              labelText: "Paasword",
-            ),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          ElevatedButton(
-            child: Text("login", style: TextStyle(fontSize: 40)),
-            style: TextButton.styleFrom(
-                minimumSize: Size(150, 100), backgroundColor: Colors.black12),
-            onPressed: () {
-              Navigator.pushNamed(context, MyRoutes.homeRoute);
-            },
-          )
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
