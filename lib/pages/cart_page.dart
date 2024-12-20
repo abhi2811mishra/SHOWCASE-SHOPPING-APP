5945784b2fc978a8640631f55013976635364e6c
abhi2811mishra <24ucc203@lnmiit.ac.in>
2024-12-20 22:44:38 +0530

    Day15 compleated

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,17 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  context.canvasColor,
+      backgroundColor: context.canvasColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
+      ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(Theme.of(context).colorScheme.secondary).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: (){},
+             style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Theme.of(context).colorScheme.secondary),
+             ),
+            child: "Buy".text.white.make(),
+            ).wh(80, 80)     
+          ],
+      ),
+    );
+  }
+}
+
+
+class _CartList extends StatefulWidget {
+
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: (){},
+          ),
+          title: "Item 1".text.make(),
       ),
     );
   }
